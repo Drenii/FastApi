9c9ab9a78a6dd74f04706d2b68e4071eea00fea3
Drenii <dreni107@hotmail.com>
2024-10-13 23:22:59 +0200

    order matters for dynamic params

--- books.py
+++ books.py
@@ -14,3 +14,12 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return{'book title': "my favorite book"}
+
+@app.get('/books/{dynamic_param}')
+async def read_all_books(dynamic_param : str):
+    return {'dynamic_param': dynamic_param}
+
