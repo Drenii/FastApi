f593f010f74e6b2cbf019386fbd9c1738e16232a
Drenii <dreni107@hotmail.com>
2024-10-14 02:04:55 +0200

    path & query parameters

--- books.py
+++ books.py
@@ -21,4 +21,22 @@ async def read_book(book_title: str):
         if book['title'].casefold() == book_title.casefold():
             return book
 
+#QUERY PARAMETER EXAMPLE
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
+#QUERY AND PATH PARAMETERS EXAMPLE
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+         book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
