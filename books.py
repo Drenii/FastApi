4e661cee32da871c09a52bea089f4d5aa34942b0
Drenii <dreni107@hotmail.com>
2024-10-14 11:48:00 +0200

    update & put methods

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app=FastAPI()
 
@@ -40,3 +40,17 @@ async def read_author_category_by_query(book_author: str, category: str):
          book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
+
+#POST METHOD
+
+@app.post('/books/create_book')
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+#PUT METHOD
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
