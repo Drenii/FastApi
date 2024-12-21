78aad64cf6e97f511be508bfbe2304be40f90e81
Drenii <dreni107@hotmail.com>
2024-12-21 20:15:26 +0100

    collect books

--- books2.py
+++ books2.py
@@ -11,13 +11,15 @@ class Book:
     author: str
     description: str
     rating: int
+    publish_date: int
 
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, publish_date: int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.publish_date = publish_date
 
 class BookRequest(BaseModel):
     id: Optional[int]= Field(description='ID is not needed on create', default=None) 
@@ -25,6 +27,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    publish_date: int = Field(gt=2000, lt=2030)
 
     model_config = {
         "json_schema_extra":{
@@ -32,18 +35,19 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "Coding with Dren",
                 "description": "This is a description for that book",
-                "rating": 5
+                "rating": 5,
+                "publish_date": 2022
             }
         }
     }
 
 
 BOOKS=[
-    Book(1, 'Book 1', 'Author 1', 'Description 1', 5),
-    Book(2, 'Book 2', 'Author 2', 'Description 2', 4),
-    Book(3, 'Book 3', 'Author 3', 'Description 3', 3),
-    Book(4, 'Book 4', 'Author 4', 'Description 4', 2),
-    Book(5, 'Book 5', 'Author 5', 'Description 5', 1)
+    Book(1, 'Book 1', 'Author 1', 'Description 1', 5, 2020),
+    Book(2, 'Book 2', 'Author 2', 'Description 2', 4, 2020),
+    Book(3, 'Book 3', 'Author 3', 'Description 3', 3, 2019),
+    Book(4, 'Book 4', 'Author 4', 'Description 4', 2, 2018),
+    Book(5, 'Book 5', 'Author 5', 'Description 5', 1, 2021)
 
 ]
 
@@ -69,6 +73,17 @@ async def read_books_by_rating(book_rating: int):
             books_to_return.append(book)
     return books_to_return
 
+#READ BOOKS BY PUBLISHED YEAR
+@app.get("/books/publish/")
+async def read_books_by_publish_date(publish_date: int):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.publish_date==publish_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+#CREATE BOOK
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book=Book(**book_request.model_dump())
